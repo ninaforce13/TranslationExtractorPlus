b3dcc4ca506255354c18d2cac026a98e43437727
NCrafters <68625676+ninaforce13@users.noreply.github.com>
2023-09-15 23:35:35 -0400

    Add files via upload
    Added new translation files

---
+++ translationextractor.gd
@@ -0,0 +1,44 @@
+extends Node
+var exclusion_folders = []
+var csv_file = File.new()
+var message_list:Dictionary = {}
+var languages = ["en", "de", "eo","es","es_mx","fr","it","ja","ko","pt","zh",]
+var display_lang = ["English", "German", "Esperanto", "Spanish", "Spanish (Mexico)","French","Italian","Japanese","Korean","Portuguese","Chinese (Traditional)"]
+onready var language_options:OptionButton = $PanelContainer/MarginContainer/GridContainer/OptionButton
+var translation_files:Array = ["res://addons/TranslationDump/1.1_demo.csv.tres",
+							   "res://addons/TranslationDump/1.1_release.csv.tres",
+								"res://addons/TranslationDump/dialogue_demo.csv.tres",
+								"res://addons/TranslationDump/dialogue_release.csv.tres",
+								"res://addons/TranslationDump/strings_demo.csv.tres",
+								"res://addons/TranslationDump/strings_release.csv.tres"
+								]
+func _ready():	
+	for language in display_lang:
+		language_options.add_item(language)
+
+func add_messages():
+	for key in message_list.keys():
+		var string:String = message_list[key]
+		var new_string:String = message_list[key]
+		new_string = new_string.replacen("\n","/!")
+		message_list[key] = new_string
+		csv_file.store_string(key + "\t" + message_list[key] + "\n")	
+
+func _on_Button_pressed():
+	$FileDialog.popup()		
+
+func _on_FileDialog_dir_selected(dir):
+	var language = languages[language_options.selected]
+	TranslationServer.set_locale(language)	
+	csv_file.open(dir+"/translation_extract_"+language+".csv", File.WRITE)
+	csv_file.store_string("id,"+language+"\n") 
+	var translationset
+	for file in translation_files:
+		translationset = load(file)		
+		for i in range(0, translationset.translations[language].messages.size(), 2):
+			if translationset.translations[language].messages[i] != "":
+				message_list[translationset.translations[language].messages[i]] = translationset.translations[language].messages[i+1]
+	add_messages()
+	csv_file.close()
+	SceneManager.change_scene("res://addons/TranslationDump/TranslationBox.tscn")
+
